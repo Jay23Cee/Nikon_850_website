73a72bafe91c2ae8442c8ca4a4b60b64ae4cc6b2
jay23cee <jayceexxiii@gmail.com>
2020-12-24 20:51:37 -0800

    added javascript

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Nikon_850_website
 
 
-**Version 1.0.0***
+**Version 1.0.1***
 
 Creating a Nikon Concept website using HTML5, CSS3 and Javascript for animations. 
 
 
+
 still needs the "About" , Gallery, and details for the intro.
+
+
+12/24/2020
+
+Added javascript.
+Currently working on making a carousel. 
+    - The left button works.
+        - image only changes once.
+            - error: is currently is js. 132
+                could be the index and it's location.
---
+++ js/script.js
@@ -0,0 +1,164 @@
+
+     var itemClassName = 'carousel_slide'
+     var items = document.getElementsByClassName(itemClassName);
+    // var carousels_slide = document.getElementsByClassName('carousel_slide carousel_slide--active'),
+
+    total_item =items.length,
+    slide = 0,
+    moving = false;
+
+    function setInitialClasses(){
+        // targets the previous, current, and next items
+        
+
+        // This assumes there are at least three items
+
+      items[total_item - 1].classList.add("prev");
+        items[0].classList.add(itemClassName+"--active");
+        items[1].classList.add("next");
+
+    }
+
+
+
+/// set event listeners
+function setEventListeners(){
+    // Get the Left button and event listiner for click
+
+    var left_button = document.getElementById("L-btn").addEventListener("click", Leftarrow);
+
+    // Get the Right button and event listiner for click
+    var right_button = document.getElementById("R-btn").addEventListener("click", Rightarrow);
+
+}
+
+
+//Function when left arrow is click
+function Leftarrow() {
+   
+    if (!moving){
+        //if it's the first slide , set as the last slide, else -1
+        if (slide === 0){
+            slide = (total_item-1);
+        }
+        else{
+            slide--;
+        }
+
+        //move carousel to updated slide
+        moveCarouselTo(slide);
+
+    }
+
+
+   
+
+}
+
+//function when right arrow is click
+function Rightarrow() {
+  
+    // check if moving
+    if (!moving){
+        //if it's the last slide reset to 0 , else +1
+        if (slide === (total_item-1)){
+            slide = 0;
+        }
+        else{
+            slide++;
+        }
+
+        //move carousel to updated slide
+        moveCarouselTo(slide);
+
+    }
+ 
+ }
+
+
+function disableInteraction(){
+    //set moving to true for the same duration as our transition
+
+    moving = true;
+
+    // set timeout runs it's function once after the given time
+    setTimeout(function(){
+        moving= false
+    },500);
+}
+
+function moveCarouselTo(slide){
+
+// check if carousel is moving, if not, allow interaction 
+if (!moving){
+    //temporarily disable ineractivity
+    disableInteraction();
+
+    //update the "old" adjacent slides with "new ones
+    var newPrevious = slide -1,
+        newNext = slide +1,
+        oldPrevious = slide -2,
+        oldNext = slide +2;
+
+
+    // check if carousel has more than three items
+    if ((total_item -1) > 1){
+        //check and updates if the new slides are out of bounds
+       
+        console.log("i'm at the part")
+        if(newPrevious <=0){
+            oldPrevious = (total_item-1);
+
+        }
+        else if(newNext >= (total_item -1)){
+            oldNext =0;
+        }
+
+        // checks and updates if slide is at the beginning/end
+        if (slide ===0){
+            newPrevious = (total_item -1);
+            oldPrevious = (total_item -2);
+            oldNext = (slide +1);
+        } else if (slide ===(total_item -1)){
+            newPrevious = (slide -1);
+            newNext = 0;
+            oldNext =1;
+        }
+
+        // Now we've worked out where we are and where we're going, 
+        //by adding / removing classes we'll trigger the transitions.
+
+        //Reset old next/prev elements to default classes
+        items[oldPrevious].className = itemClassName;      
+        items[oldNext].className = itemClassName;
+
+    
+        // add new classes
+        items[newPrevious].className = itemClassName + " prev";
+        items[slide].className = itemClassName + "--active";
+        items[newNext].className = itemClassName + " next";
+       
+    }
+
+
+}
+
+}
+
+
+function initCarousel(){
+  
+    setInitialClasses;
+    setEventListeners;
+
+    //set moving to false so that the carousel becomes interactive
+    moving = false;
+}
+
+
+setInitialClasses()
+var left_button = document.getElementById("L-btn").addEventListener("click", Leftarrow);
+
+// Get the Right button and event listiner for click
+var right_button = document.getElementById("R-btn").addEventListener("click", Rightarrow);
+
